0be4776593686786045b9ab2329860f1d896fde7
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-05-26 23:52:05 -0400

    almost done

--- assets/javascript/trainJS.js
+++ assets/javascript/trainJS.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
     var config = {
         apiKey: "AIzaSyCc-YOZcerl9v5B7QY6bf82NXqWL1uGArU",
         authDomain: "train-time-a7eee.firebaseapp.com",
@@ -24,144 +23,62 @@ $(document).ready(function(){
             firstTrain: $("#time-input").val()
         }
 
-        databaseRef.push(newTrain);
-        })
+        if (newTrain.trainName && newTrain.destination && newTrain.freq && newTrain.firstTrain) {
+            databaseRef.push(newTrain);
+            $("input").val("");
+        } else {
+            alert("Please fill in all fields.");
+        }
 
-    //Get new firebase data and put to html
+
+    })
+
+    //Accept Enter as a Submit Button
+    $(document).on("keypress",function(){
+        if (event.keyCode === 13) {
+            $("#submitbutton").click();
+        }
+    })
+
+    //Getting data from Firebase and adding to HTML
     databaseRef.on("child_added",function(childSnapshot){
         var nameOutput = childSnapshot.val().trainName;
         var destinationOutput = childSnapshot.val().destination;
         var freqOutput = childSnapshot.val().freq;
         var firstTrainOutput = childSnapshot.val().firstTrain;
-        //var nextArrivalOutpot = currenttime - freqOutput;
-        //var minutesAwayOutput = currenttime - nextArrivalOutpot;
+       
+
+        var firstTimeConverted = moment(firstTrainOutput, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+    
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % freqOutput;
+        console.log(tRemainder);
+
+        // Minutes Until Next Train
+        var minutesAwayOutput = freqOutput - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + minutesAwayOutput);
+
+        // Next Train
+        var nextArrivalOutput = moment().add(minutesAwayOutput, "minutes").format("hh:mm a");
+        console.log("ARRIVAL TIME: " + moment(nextArrivalOutput).format("hh:mm a"));
 
         var newRow = $("<tr>");
             var nameRow = $("<td>").text(nameOutput).appendTo(newRow);
             var destinationRow = $("<td>").text(destinationOutput).appendTo(newRow);
             var freqRow = $("<td>").text(freqOutput).appendTo(newRow);
-            //var nextArrivalRow = $("<td>").text(nextArrivalOutpot).appendTo(newRow);
-            //var minutesAwayRow = $("<td>").text(minutesAwayOutput).appendTo(newRow);
+            var nextArrivalRow = $("<td>").text(nextArrivalOutput).appendTo(newRow);
+            var minutesAwayRow = $("<td>").text(minutesAwayOutput).appendTo(newRow);
         
-        $(".rows-go-here").append(newRow);
-
+        $(".rows-go-here").prepend(newRow);
     })
-
-      /*      //MOMENT EXAMPLE
-        
-
-            var randomDate = "02/23/1999";
-            var randomFormat = "MM/DD/YYYY";
-            var convertedDate = moment(randomDate, randomFormat);
-
-            // Using scripts from moment.js write code below to complete each of the following.
-            // Console.log to confirm the code changes you made worked.
-
-            // 1 ...to convert the randomDate into three other date formats
-            console.log(moment(convertedDate).format("MM/DD/YY"));
-            console.log(moment(convertedDate).format("MMM Do, YYYY hh:mm:ss"));
-            console.log(moment(convertedDate).format("X"));
-            console.log("----------------------------------------");
-
-            // 2 ...to determine the time in years, months, days between today and the randomDate
-            console.log(moment(convertedDate).toNow());
-            console.log(moment(convertedDate).diff(moment(), "years"));
-            console.log(moment(convertedDate).diff(moment(), "months"));
-            console.log(moment(convertedDate).diff(moment(), "days"));
-            console.log("----------------------------------------");
-
-            // 3 ...to determine the number of days between the randomDate and 02/14/2001
-            var newDate = moment("02/14/2001", randomFormat);
-            console.log(moment(convertedDate).diff(moment(newDate), "days"));
-
-            // 4 ...to convert the randomDate to unix time (be sure to look up what unix time even is!!!)
-            console.log(moment(convertedDate).format("X"));
-            console.log("----------------------------------------");
-
-            // 5 ...to determine what day of the week and what week of the year this randomDate falls on.
-            console.log(moment(convertedDate).format("DDD"));
-            console.log(moment(convertedDate).format("dddd"));
-
-            //TIMESHEET EXAMPLE
-            // 2. Button for adding Employees
-                    $("#add-employee-btn").on("click", function(event) {
-                        event.preventDefault();
-                    
-                        // Grabs user input
-                        var empName = $("#employee-name-input").val().trim();
-                        var empRole = $("#role-input").val().trim();
-                        var empStart = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
-                        var empRate = $("#rate-input").val().trim();
-                    
-                        // Creates local "temporary" object for holding employee data
-                        var newEmp = {
-                        "name": empName,
-                        "role": empRole,
-                        "start": empStart,
-                        "rate": empRate
-                        };
-                    
-                        // Uploads employee data to the database
-                        database.ref("/employees").push(newEmp);
-                    
-                        // Logs everything to console
-                        console.log(newEmp.name);
-                        console.log(newEmp.role);
-                        console.log(newEmp.start);
-                        console.log(newEmp.rate);
-                    
-                        // Alert
-                        alert("Employee successfully added");
-                    
-                        // Clears all of the text-boxes
-                        $("#employee-name-input").val("");
-                        $("#role-input").val("");
-                        $("#start-input").val("");
-                        $("#rate-input").val("");
-                    });
-                    
-                    // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-                    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-                    
-                        console.log(childSnapshot.val());
-                    
-                        // Store everything into a variable.
-                        var empName = childSnapshot.val().name;
-                        var empRole = childSnapshot.val().role;
-                        var empStart = childSnapshot.val().start;
-                        var empRate = childSnapshot.val().rate;
-                    
-                        // Employee Info
-                        console.log(empName);
-                        console.log(empRole);
-                        console.log(empStart);
-                        console.log(empRate);
-                    
-                        // Prettify the employee start
-                        var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
-                    
-                        // Calculate the months worked using hardcore math
-                        // To calculate the months worked
-                        var empMonths = moment().diff(moment(empStart, "X"), "months");
-                        console.log(empMonths);
-                    
-                        // Calculate the total billed rate
-                        var empBilled = empMonths * empRate;
-                        console.log(empBilled);
-                    
-                        // Add each train's data into the table
-                        $("#employee-table > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" +
-                        empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
-                    });
-                    
-                    // Example Time Math
-                    // -----------------------------------------------------------------------------
-                    // Assume Employee start date of January 1, 2015
-                    // Assume current date is March 1, 2016
-                    
-                    // We know that this is 15 months.
-                    // Now we will create code in moment.js to confirm that any attempt we use meets this test case
-*/
 
 })
     
